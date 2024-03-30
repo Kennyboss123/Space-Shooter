5b98ad388da6f1f56857df97149448e64c14cfc2
DESKTOP-LQ2DMJ7\USER <ikpechukwukamsy@gmail.com>
2024-03-29 18:36:36 -0700

    Update

--- Assets/Scripts/SpaceLaser.cs
+++ Assets/Scripts/SpaceLaser.cs
@@ -7,6 +7,6 @@ public class SpaceLaser : MonoBehaviour
     float moveSpeed = 4f;
     private void Update()
     {
-        gameObject.transform.Translate(new Vector2(0, 5f) * moveSpeed * Time.deltaTime, Space.Self);
+        gameObject.transform.Translate(new Vector2(0, 4f) * moveSpeed * Time.deltaTime, Space.Self);
     }
 }
